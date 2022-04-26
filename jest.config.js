bfe9f77867d93b29b1b9be8b9b1d1f713e5fa6f5
Jiří Špác <capajj@gmail.com>
2022-04-26 09:42:29 +0200

    fix jest execution

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ export default {
   modulePaths: ['<rootDir>/src/'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   collectCoverage: false,
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   resolver: 'jest-ts-webcompat-resolver',
   testMatch: ['**/*.spec.(ts|tsx|js)'],
   setupFiles: ['<rootDir>/src/specs/setup.ts'],
-  testPathIgnorePatterns: ['/lib/'],
   globals: {
     'ts-jest': {
       diagnostics: false,
